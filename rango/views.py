2ffb86ec33e9ed75f9d4cb741a9ea36720b3ff74
Dillan Cowell <2913430c@student.gla.ac.uk>
2025-02-13 16:03:49 +0000

    Added show_category view for individual vategory pages and added a response for the top 5 categories and pages to be displayed on the index page.

--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category, Page
 
 def index(request):
-    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by("-likes")[:5] # Retrieve the top 5 most liked categories
+    page_list = Page.objects.order_by("-views")[:5] # Likewise for pages
+    
+    context_dict = {
+        "boldmessage": "Crunchy, creamy, cookie, candy, cupcake!",
+        "categories": category_list,
+        "pages": page_list
+    }
 
     return render(request, "rango/index.html", context=context_dict)
 
 def about(request):
     context_dict = {"indexlink": "/rango/"}
 
-    return render(request, "rango/about.html", context=context_dict)
+    return render(request, "rango/about.html", context=context_dict)#
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+        
+        context_dict["category"] = category
+        context_dict["pages"] = pages
+    except Category.DoesNotExist:
+        context_dict["category"] = None
+        context_dict["pages"] = None
+    
+    return render(request, "rango/category.html", context=context_dict)

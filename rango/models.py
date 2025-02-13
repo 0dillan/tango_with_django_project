3eed68eb08e333bf16d671514739237044ec0904
Dillan Cowell <2913430c@student.gla.ac.uk>
2025-02-13 13:54:13 +0000

    Changed the list display of the Page attributes on the admin page.

--- rango/models.py
+++ rango/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     # Makes the name of the Category model plural when used in a plural context
     class Meta:
